ddf84aec15be28d66e3ccbcffeca16bb25c5eab3
epix37 <epikz37@gmail.com>
2014-09-13 11:08:48 +0200

    stats are recorded if no deck is selected

--- Hearthstone Deck Tracker/Stats/DeckStats.cs
+++ Hearthstone Deck Tracker/Stats/DeckStats.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Xml.Serialization;
 
 namespace Hearthstone_Deck_Tracker.Stats
@@ -30,37 +32,6 @@ namespace Hearthstone_Deck_Tracker.Stats
 		public void AddGameResult(GameStats gameStats)
 		{
 			Games.Add(gameStats);
-		}
-	}
-
-	public class DeckStatsList
-	{
-		private static DeckStatsList _instance;
-
-		[XmlArray(ElementName = "DeckStats")]
-		[XmlArrayItem(ElementName = "Deck")]
-		public List<DeckStats> DeckStats;
-
-		public DeckStatsList()
-		{
-			DeckStats = new List<DeckStats>();
-		}
-
-		public static DeckStatsList Instance
-		{
-			get { return _instance ?? (_instance = new DeckStatsList()); }
-		}
-
-		public static void Load()
-		{
-			var file = Config.Instance.DataDir + "DeckStats.xml";
-			_instance = XmlManager<DeckStatsList>.Load(file);
-		}
-
-		public static void Save()
-		{
-			var file = Config.Instance.DataDir + "DeckStats.xml";
-			XmlManager<DeckStatsList>.Save(file, Instance);
 		}
 	}
 }
